cce5cc10ea6813ab799c9f167e4f9851fe2c3a94
Atul R <atulanand94@gmail.com>
2019-09-22 19:35:21 +0200

    removes unnecessary files

--- config/application.gypi
+++
@@ -1,6 +0,0 @@
-{
-    "sources": [
-        "../src/cpp/main.cpp",
-        "../src/cpp/QStatusBar/qstatusbar_wrap.cpp",
-    ],
-}
--- config/moc.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  rootDir: path.resolve(__dirname, ".."),
-  include: "",
-  headers: ["src/cpp/QStatusBar/nstatusbar.h"]
-};
