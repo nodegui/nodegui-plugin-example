4d70a7c67bb57a991b66d059f97beccd32fd7daa
master-atul <atulanand94@gmail.com>
2020-01-22 21:21:12 +0100

    cleanup

--- src/cpp/QStatusBar/nstatusbar.hpp
+++ src/cpp/QStatusBar/nstatusbar.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <nodegui/core/NodeWidget/nodewidget.h>
-
 #include <QStatusBar>
 
 class NStatusBar : public QStatusBar, public NodeWidget {
+ public:
   Q_OBJECT
   NODEWIDGET_IMPLEMENTATIONS(QStatusBar)
  public:
